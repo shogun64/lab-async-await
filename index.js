d39fb5c75bbfff6ed3efa1d7656ed619629f1fe0
Ian Synge <gamersynge@gmail.com>
2025-06-17 20:51:42 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+function displayPosts(posts){
+    const postList = document.getElementById("post-list");
+    posts.forEach((post) => {
+        const list = document.createElement("li");
+        const header = document.createElement("h1");
+        header.textContent = post.title;
+        const paragraph = document.createElement("p");
+        paragraph.textContent = post.body;
+
+        list.append(header);
+        list.append(paragraph);
+        postList.append(list);
+    })
+}
+
+async function retrievePosts(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    displayPosts(posts);
+}
+
+retrievePosts();
